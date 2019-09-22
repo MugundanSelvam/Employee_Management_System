436ae600f96c3d5eb60d04150a0b30c4bdaf1fbc
MugundanSelvam <mugu2804@gmail.com>
2019-09-22 15:02:38 +0530

    First Commit

---
+++ src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,28 @@
+import { Router } from '@angular/router';
+import { EmployeeService } from './../employee.service';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../Employee';
+
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit {
+ 
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService, private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  submit(employee):void{
+    this.employeeService.addEmployee(employee as Employee).subscribe(employee => {
+      this.employees.push(employee);
+     });
+     this.router.navigate(['/employees']);
+  }
+
+}
---
+++ src/app/employee-details/employee-details.component.ts
@@ -0,0 +1,47 @@
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../Employee';
+import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-employee-details',
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.css']
+})
+export class EmployeeDetailsComponent implements OnInit {
+  
+  @Input() employee: Employee; 
+
+  
+
+  constructor(private route: ActivatedRoute,
+    private employeeService: EmployeeService,
+    private location: Location) {
+   }
+
+  ngOnInit() {
+    this.getEmployee();
+  }
+
+  getEmployee(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.employeeService.getEmployee(id)
+      .subscribe(employee => this.employee= employee);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
